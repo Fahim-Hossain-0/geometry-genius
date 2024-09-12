e727a0fa80a077659b1d28b54425f57f5e680919
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2024-09-12 20:09:39 +0600

    first commit

---
+++ script/triangle.js
@@ -0,0 +1,11 @@
+function calculateTriangleArea() {
+    const triangleBaseInput = document.getElementById("triangle-base")
+    const triangleBaseText = triangleBaseInput.value
+    const base = parseFloat(triangleBaseText)
+    console.log(base)
+
+    const triangleHightInput = document.getElementById("triangle-hight")
+    const triangleHightText = triangleHightInput.value;
+    const hight = parseFloat(triangleHightText)
+    console.log(hight)
+}
